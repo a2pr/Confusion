f84b5afb75be1a1f1a38a34155239887a178a3d1
andres <a2payema@gmail.com>
2018-01-21 22:34:56 -0400

    adding fix for build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import{MdDialog, MdDialogRef} from '@angular/material';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  user={remember:false};
+  user={username:'',password:'',remember:false};
   constructor(private dialogRef: MdDialogRef<LoginComponent>) { }
 
   ngOnInit() {
